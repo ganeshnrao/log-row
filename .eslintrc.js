735f41965b6c710cff5ccde53531a15ccd977fee
Ganesh Rao <ganeshnrao@me.com>
2019-05-14 11:24:20 -0700

    Add eslint script, replace image

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-console": "off"
+  }
 };
